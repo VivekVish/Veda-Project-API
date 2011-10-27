1cfd731fbf488db9cdcbd9b13c7b24cc7199eab6
Vivek Viswanathan <viswanathan@vedaproject.org>
2011-10-27 01:09:30 -0400

    Changed HTML Purifier to accept info box attributes

--- includes/htmlpurifier.php
+++ includes/htmlpurifier.php
@@ -1,7 +1,7 @@
 <?php
     require_once("includes/htmlpurifier/library/HTMLPurifier.auto.php");
     $HTML_Allowed_Elms = 'section,p,ul,ol,li,span,div,h2,h3,h4,h5,h6,h7,table,tbody,tr,td,th,blockquote,br';
-    $HTML_Allowed_Classes = 'ilo';
+    $HTML_Allowed_Classes = 'ilo,infoBox';
     
     $purifierConfig = HTMLPurifier_Config::createDefault();
 
@@ -14,6 +14,7 @@
     $purifierConfig->set('HTML.AllowedElements', $HTML_Allowed_Elms);
     $def = $purifierConfig->getHTMLDefinition(true);
     $def->addAttribute('div','data-ilotype','Text');
+    $def->addAttribute('div','data-infoboxtype','Text');
     $def->addAttribute('span','data-ilotype','Text');
     $htmlSection = $def->addElement(
                                     'section',
